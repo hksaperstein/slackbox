349f93339ace57d44245eda3bfccd6198e62802a
Harrison Saperstein <harrison.saperstein@gmail.com>
2022-05-30 22:29:24 -0400

    end point works

--- src/slackbot.py
+++ src/slackbot.py
@@ -15,17 +15,21 @@ app = Flask(__name__)
 
 # Slack initiation
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+BOT_ID = client.api_call('auth.test')['user_id']
 slack_event_adapter = SlackEventAdapter(
     os.environ['SIGNING_SECRET'],'/slack/events', app)
 
+
 @slack_event_adapter.on('message')
 def message(payload):
+    print("here")
+
     event = payload.get('event', {})
     channel_id = event.get('channel')
     user_id = event.get('user')
-    text = event.get('test')
-
-    client.chat_postMessage(channel=channel_id, text=text)
+    text = event.get('text')
+    if not BOT_ID == user_id:
+        client.chat_postMessage(channel=channel_id, text=text)
 
     # Start Webserver
 
